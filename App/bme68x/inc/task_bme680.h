53f4b682b523eb9f376829fc6019dfb44e38cd8f
Yuval Kuck <kuckng@gmail.com>
2026-07-22 16:55:24 +0300

    some renames

---
+++ App/bme68x/inc/task_bme680.h
@@ -0,0 +1,17 @@
+//
+// Created by uv on 22/07/2026.
+//
+
+#ifndef CLEARAIR_TASK_BME680_H
+#define CLEARAIR_TASK_BME680_H
+#include "stm32f4xx_hal_i2c.h"
+
+
+// class TaskBme680 {
+// public:
+//     TaskBme680(
+//     I2C_HandleTypeDef* hi2c,
+//     uint8_t            i2c_addr8,   // 8-bit shifted address
+//     QueueHandle_t      output_queue)
+// };
+#endif //CLEARAIR_TASK_BME680_H
