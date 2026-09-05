c796a32a8cf27d07d050d87d04d60e7df3361e17
Yuval Kuck <kuckng@gmail.com>
2026-09-05 22:14:52 +0300

    validate particles communicate

--- device/particle/src/task_particle.cpp
+++ device/particle/src/task_particle.cpp
@@ -5,7 +5,6 @@
 #include "stm32f4xx.h"
 #include "task_particle.h"
 #include "sensirion_common.h"
-#include <stdio.h>
 #include "bridge_particle.h"
 #include "sps30_i2c.h"
 #include "sensirion_i2c_hal.h"
@@ -13,36 +12,6 @@
 #include "event_message.h"
 #include "logger.h"
 
-// #include "timestamp.h"
-
-// static uint8_t work_buffer[BSEC_MAX_WORKBUFFER_SIZE];
-// static bme68x_dev commBridgeCfg = {0};
-// static constexpr uint32_t BSEC_TASK_MAX_WAIT_MS     = 2000; // BME68x max heater dur
-// #define BSEC_REQUESTED_OUTPUTS 6
-//
-// static auto applyBsecSensorSettings(const bsec_bme_settings_t& settings) {
-//     bme68x_conf conf{};
-//     bme68x_heatr_conf heatr_conf{};
-//
-//     auto rc = bme68x_get_conf(&conf, &commBridgeCfg);
-//     if (rc != BME68X_OK) { return rc; }
-//
-//     conf.os_hum = settings.humidity_oversampling;
-//     conf.os_temp = settings.temperature_oversampling;
-//     conf.os_pres = settings.pressure_oversampling;
-//     rc = bme68x_set_conf(&conf, &commBridgeCfg);
-//     if (rc != BME68X_OK) { return rc; }
-//
-//     heatr_conf.enable = BME68X_ENABLE;
-//     heatr_conf.heatr_temp = settings.heater_temperature;
-//     heatr_conf.heatr_dur = settings.heater_duration;
-//     rc = bme68x_set_heatr_conf(BME68X_FORCED_MODE, &heatr_conf, &commBridgeCfg);
-//     if (rc != BME68X_OK) { return rc; }
-//
-//     rc = bme68x_set_op_mode(BME68X_FORCED_MODE, &commBridgeCfg);
-//     return rc;
-// }
-//
 bool SensorParticle::configure(I2C_HandleTypeDef* hi2c) {
     METHODTRACE
     initBridgeParticle(hi2c, SPS30_I2C_ADDR_69);
@@ -51,17 +20,18 @@ bool SensorParticle::configure(I2C_HandleTypeDef* hi2c) {
     if ( rc != NO_ERROR) {
         METHODLOG(fatal, "sps30_stop_measurement failed")
     }
-    // int8_t serial_number[32] = {0};
-    // int8_t product_type[8] = {0};
-    // rc = sps30_read_serial_number(serial_number, 32);
-    // printf("serial_number: %p\n", serial_number);
-    // rc = sps30_read_product_type(product_type, 8);
-    // printf("product_type: %p\n", product_type);
+#if defined(DEBUG)
+    int8_t serial_number[32] = {0};
+    int8_t product_type[8] = {0};
+    rc = sps30_read_serial_number(serial_number, 32);
+    METHODLOGS(trace, "sps30_read_serial_number(): %p", serial_number);
+    rc = sps30_read_product_type(product_type, 8);
+    METHODLOGS(trace, "product_type: %p", product_type);
+#endif
 
     return (rc == 0);
 }
 
-//
 constexpr auto PARTICAL_CYCLE_TIME_MS = 60 * 1000;
 constexpr auto PARTICAL_ONERROR_CYCLE_MS = 10 * 1000;
 static CommonMessage msg{};
